c318b6b5b8cb1e9b5f5dd0c9862761cbbcb36463
Shagy Jimmy <60968628+ShagyJimmy@users.noreply.github.com>
2020-08-30 18:33:23 +0300

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
-const String TITLE = "ProPakistani";
+const String TITLE = "Kizobraxmusic";
 
 /// last slash ' / ' is mandatory
-const String URL = "https://propakistani.pk/";
+const String URL = "https://kizobraxmusic.com/"
 
 const int FEATURED_CATEGORY_ID = 20;
 const String FEATURED_CATEGORY_TITLE = 'Featured';
